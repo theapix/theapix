3fbe37c7f0477355793fa63a8e803411a59a8875
soGit <soroka.sergei@gmail.com>
2024-09-01 11:04:28 +0100

    Update landing page and links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://theapix.github.io',
-	base: 'theapix',
+	site: 'https://theapix.com',
+	base: '',
 	integrations: [mdx(), sitemap()],
 });
